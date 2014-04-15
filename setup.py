1bcb6cb79a944fda4aa47ac37217275268a248b5
Oliver <oliver@alpuca.com>
2014-04-15 12:08:32 +0100

    Updated package files

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+EVODjango Django extensions for development aid
+===============================================
 
-from setuptools import setup
+.. module:: pyevo
+    :platform: Unix, Windows
+    :synopsis: EVODjango Django extensions for development aid
+.. moduleauthor:: (C) 2013 Oliver Gutiérrez
+"""
+# Python imports
+import os
+
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="evodjango",
+    name='evodjango',
+    packages = find_packages(),
     version='0.1',
-    description='Django extensions for development aid',
+    description='EVODjango Django extensions for development aid',
+    long_description=README,
+    license='MIT License',
     author="Oliver Gutiérrez",
     author_email="ogutsua@gmail.com",
-    packages =['evodjango'],
-    platforms=['any'],
+    url = 'https://github.com/R3v1L/evodjango',
+    keywords = ['evodjango', 'django', 'utility', ],
+    classifiers = [],
+    # download_url = '',
 )
