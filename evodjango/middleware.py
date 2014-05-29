702247d24083a571ec912c933cf27bb59988298d
Oliver Gutiérrez <oliver@alpuca.com>
2014-05-29 11:30:31 +0100

    Changed SEOFriendlyLocaleMiddleware to use the new schema in django 1.8

--- evodjango/middleware.py
+++ evodjango/middleware.py
@@ -39,27 +39,4 @@ class SEOFriendlyLocaleMiddleware(LocaleMiddleware):
     """
     SEO Friendly locale middleware
     """
-    def process_response(self, request, response):
-        """
-        Response processing
-        """
-        language = translation.get_language()
-        if (response.status_code == 404 and
-                not translation.get_language_from_path(request.path_info)
-                    and self.is_language_prefix_patterns_used()):
-            urlconf = getattr(request, 'urlconf', None)
-            language_path = '/%s%s' % (language, request.path_info)
-            if settings.APPEND_SLASH and not language_path.endswith('/'):
-                language_path = language_path + '/'
-
-            if is_valid_path(language_path, urlconf):
-                language_url = "%s://%s/%s%s" % (
-                    request.is_secure() and 'https' or 'http',
-                    request.get_host(), language, request.get_full_path())
-                return HttpResponsePermanentRedirect(language_url)
-        translation.deactivate()
-
-        patch_vary_headers(response, ('Accept-Language',))
-        if 'Content-Language' not in response:
-            response['Content-Language'] = language
-        return response
+    response_redirect_class = HttpResponsePermanentRedirect
